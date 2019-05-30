a1c730ea5bb5c11f2777e97b2f5ff2ee34f97272
Mohamud Olow <khalif18@live.com>
2019-05-29 18:57:19 -0700

    add container class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Container } from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,7 +8,9 @@ class App extends React.Component {
   }
   render () {
     return (
+      <Container>
       <h1>Tip Calculator</h1>
+      </Container>
     );
   }
 }
