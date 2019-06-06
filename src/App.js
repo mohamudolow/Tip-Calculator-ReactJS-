7f638d8df6f2afd2795297de70cd72879f2c8de5
Mohamud Olow <khalif18@live.com>
2019-06-05 21:14:44 -0700

    code indented. added validations for tip and numOfPeople values in the handleIncrement method

--- src/App.js
+++ src/App.js
@@ -11,38 +11,38 @@ class PersonIncrementor extends React.Component {
     return (
       <div className="incrementor">
       <i className="fas fa-user-times" onClick={()=> this.props.handleIncrement('numOfPeople', -1)}></i>&nbsp;&nbsp;
-      <span>{this.props.numOfPeople}</span>&nbsp;&nbsp;
-      <i className="fas fa-user-plus" onClick={()=> this.props.handleIncrement('numOfPeople', 1)}></i></div>
-    );
-  }
+<span>{this.props.numOfPeople}</span>&nbsp;&nbsp;
+<i className="fas fa-user-plus" onClick={()=> this.props.handleIncrement('numOfPeople', 1)}></i></div>
+  );
+}
 }
 
 
 const Bill = (props) => {
   return (
     <div className="bill">
-      <Form>
+    <Form>
     <Row>
-      <Col><Form.Label>Bill</Form.Label></Col>
+    <Col><Form.Label>Bill</Form.Label></Col>
     <Col>
     <p>Number of people</p>
     </Col>
     </Row>
-      <Row>
-      <Col>
-      <InputGroup className="input1">
-      <InputGroup.Prepend>
-      <InputGroup.Text>$</InputGroup.Text>
-      </InputGroup.Prepend>
-      <FormControl arai-label="Amount (to the nearest two decimal places)" value={props.bill} onChange={props.handleChange}/>
-      </InputGroup>
-      </Col>
-      <Col>
+    <Row>
+    <Col>
+    <InputGroup className="input1">
+    <InputGroup.Prepend>
+    <InputGroup.Text>$</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl arai-label="Amount (to the nearest two decimal places)" value={props.bill} onChange={props.handleChange}/>
+    </InputGroup>
+    </Col>
+    <Col>
     <PersonIncrementor numOfPeople={props.numOfPeople} handleIncrement={props.handleIncrement}/>
     </Col>
-      </Row>
-      </Form>
-      </div>
+    </Row>
+    </Form>
+    </div>
   );
 }
 
@@ -51,7 +51,7 @@ class ServiceQuality extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
       <Carousel>
@@ -60,43 +60,43 @@ class ServiceQuality extends React.Component {
       className="d-block w-100" src="https://cdn.pixabay.com/photo/2012/03/01/00/57/background-19861_1280.jpg"
       alt="First slide"
       onClick={()=> this.props.service('tip', 35)}/> 
-      <Carousel.Caption>
+<Carousel.Caption>
       <h3>Excellent</h3>
-      </Carousel.Caption>
-      </Carousel.Item>
+</Carousel.Caption>
+</Carousel.Item>
 
-      <Carousel.Item>
+<Carousel.Item>
       <img
-      className="d-block w-100"
-      src="https://images.unsplash.com/photo-1533035336122-4327d347d2fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-      alt="Second slide"
-      onClick={()=> this.props.service('tip', 25)}/>
-      <Carousel.Caption>
-      <h3>Good</h3>
-      </Carousel.Caption>
-      </Carousel.Item>
+className="d-block w-100"
+src="https://images.unsplash.com/photo-1533035336122-4327d347d2fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+alt="Second slide"
+onClick={()=> this.props.service('tip', 25)}/>
+  <Carousel.Caption>
+  <h3>Good</h3>
+</Carousel.Caption>
+</Carousel.Item>
 
-      <Carousel.Item>
-      <img
-      className="d-block w-100"  src="https://cdn.pixabay.com/photo/2012/12/24/08/39/backdrop-72250_1280.jpg"
-      alt="Third slide" onClick={() => this.props.service('tip', 15)} />
-      <Carousel.Caption>
-      <h3>Fair</h3>
-      </Carousel.Caption>
-      </Carousel.Item>
+<Carousel.Item>
+  <img
+className="d-block w-100"  src="https://cdn.pixabay.com/photo/2012/12/24/08/39/backdrop-72250_1280.jpg"
+alt="Third slide" onClick={() => this.props.service('tip', 15)} />
+  <Carousel.Caption>
+  <h3>Fair</h3>
+</Carousel.Caption>
+</Carousel.Item>
 
-      <Carousel.Item>
-      <img
-      className="d-block w-100"
-      src="https://images.unsplash.com/photo-1538635546732-684a9ec39ea5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1070&q=80"
-      alt="Fourth slide" onClick={() => this.props.service('tip', 5)} />
-      <Carousel.Caption>
-      <h3>Poor</h3>
-      </Carousel.Caption>
-      </Carousel.Item>
-      </Carousel>
-    );
-  }
+<Carousel.Item>
+  <img
+className="d-block w-100"
+src="https://images.unsplash.com/photo-1538635546732-684a9ec39ea5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1070&q=80"
+alt="Fourth slide" onClick={() => this.props.service('tip', 5)} />
+  <Carousel.Caption>
+  <h3>Poor</h3>
+</Carousel.Caption>
+</Carousel.Item>
+</Carousel>
+);
+}
 };
 
 
@@ -108,11 +108,11 @@ class TipIncrementor extends React.Component {
     return (
       <div className="incrementor">
       <i className="fas fa-minus-square" onClick={() => this.props.handleIncrement('tip', -1)}></i>&nbsp;&nbsp;
-      <span>{this.props.tip}</span>&nbsp;&nbsp;
-      <i className="fas fa-plus-square" onClick={()=> this.props.handleIncrement('tip', 1)}></i>
-      </div>
-    );
-  }
+<span>{this.props.tip}</span>&nbsp;&nbsp;
+<i className="fas fa-plus-square" onClick={()=> this.props.handleIncrement('tip', 1)}></i>
+</div>
+);
+}
 };
 
 
@@ -121,35 +121,35 @@ const Result = (props) => {
   const ITWithTip = Math.round((ITWithoutTip + (props.tip/100 * props.bill/props.numOfPeople)) * 100)/100;
   const tipTotal = Math.round((ITWithTip -ITWithoutTip) * props.numOfPeople *100)/100;
   const tipPerPerson = Math.round(tipTotal / props.numOfPeople * 100)/100;
-  
+
   return (
     <div id="result">
     <Container>
-      <Row>
-      <Col>Individual total without tip:</Col>
-      <Col className="num">{ITWithoutTip}</Col>
-      </Row>
-      
-      <Row>
-      <Col>Individual total with tip:</Col>
-      <Col className="num">{ITWithTip}</Col>
-      </Row>
-      
-      <Row>
-      <Col>Total tip amount:</Col>
-      <Col className="num">{tipTotal}</Col>
-      </Row>
+    <Row>
+    <Col>Individual total without tip:</Col>
+    <Col className="num">{ITWithoutTip}</Col>
+    </Row>
+
+    <Row>
+    <Col>Individual total with tip:</Col>
+    <Col className="num">{ITWithTip}</Col>
+    </Row>
+
+    <Row>
+    <Col>Total tip amount:</Col>
+    <Col className="num">{tipTotal}</Col>
+    </Row>
     </Container><br />
-      
-      <div id="tip-per-person">
+
+    <div id="tip-per-person">
     <Container>
-      <Row>
-      <Col className="tip-per-person-text">Tip per person:</Col>
-      <Col className="num">{tipPerPerson}</Col>
-      </Row>
+    <Row>
+    <Col className="tip-per-person-text">Tip per person:</Col>
+    <Col className="num">{tipPerPerson}</Col>
+    </Row>
     </Container>
-      </div>
-      </div>
+    </div>
+    </div>
   );
 }
 
@@ -166,40 +166,48 @@ class App extends React.Component {
     this.handleIncrement = this.handleIncrement.bind(this);
     this.service = this.service.bind(this);
   }
-  
+
   handleChange(event) {
     this.setState({
       bill: event.target.value
     })
   }
-  
+
   handleIncrement(count, value) {
-    this.setState(prevState => ({
-                  [count]: prevState[count] + value
-                  }));
+    this.setState(prevState => {
+      if(prevState[count] > 1 || value == 1) {
+        return ({
+          [count]: prevState[count] + value
+        });
+      } else {
+        return ({
+          [count]: 20
+        });
+      }
+    });
   }
-  
+
   service(name, value) {
     this.setState({
       [name]: value
     });
   }
-  
+
   render () {
     return (
       <div id="container">
-      
+
       <div id="header">
       <Container>
       <h1>Tip Calculator</h1>
-</Container>
+      </Container>
       </div>
-      
+
       <div id="main">
       <Container>
       <Bill bill={this.state.bill} numOfPeople={this.state.numOfPeople} handleChange={this.handleChange} handleIncrement={this.handleIncrement}/>
-      
-      
+
+
       <div id="service">
       <hr />
       </div>
@@ -209,23 +217,23 @@ class App extends React.Component {
       <Col>
       <p className="experience">How was your experience today?</p>
       </Col>
-      
+
       <ServiceQuality service={this.service}/>
-      
+
       </Col>
       <Col id="tip">
       <Col><p>Tip %</p></Col><br />
       <Col>
-      
+
       <TipIncrementor handleIncrement={this.handleIncrement} tip={this.state.tip} />
-      
+
       </Col>
       </Col>
       </Row>
       </div>
       <hr />
       </Container>
-      
+
       <Result bill={this.state.bill} numOfPeople={this.state.numOfPeople} tip={this.state.tip} />
       </div>
       </div>
