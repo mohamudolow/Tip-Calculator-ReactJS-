82e69675274a10bf0fc647456af93ce79ccca50d
Mohamud Olow <khalif18@live.com>
2019-08-10 16:28:59 -0700

    Updates

---
+++ precache-manifest.59c3d2d019cc98033731987615a73c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb5dacb4fc938885789c768906b6479",
+    "url": "/Tip-Calculator-ReactJS-/index.html"
+  },
+  {
+    "revision": "a56f61e30ea1f6ad97f8",
+    "url": "/Tip-Calculator-ReactJS-/static/css/main.7f79e4d4.chunk.css"
+  },
+  {
+    "revision": "c8bfaf872ac387281c84",
+    "url": "/Tip-Calculator-ReactJS-/static/js/2.3e7063f4.chunk.js"
+  },
+  {
+    "revision": "a56f61e30ea1f6ad97f8",
+    "url": "/Tip-Calculator-ReactJS-/static/js/main.9dd65427.chunk.js"
+  },
+  {
+    "revision": "1904bb4e19f7e168f6e4",
+    "url": "/Tip-Calculator-ReactJS-/static/js/runtime~main.e4a725c6.js"
+  }
+]);
